2c4b145c6cf1b4fd54260bb78f8c4c4b7f648f52
congquyenpro <quyen.nc207699@sis.hust.edu.vn>
2023-05-08 20:02:25 +0700

    update 8/5

--- src/loop.php
+++ src/loop.php
@@ -2,4 +2,26 @@
 for ($x = 0; $x <= 10; $x++) {
   echo "The number is: $x <br>";
 }
+$servername = "db";
+$username = "root";
+$password = "mypassword";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO members (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
